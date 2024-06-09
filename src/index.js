a84aa290f6198c123e90a81e14071fcaa52c4197
Vikram-Raj-Purohit <vikram2206raj@gmail.com>
2024-06-09 12:13:31 +0530

    changes

---
+++ src/index.js
@@ -0,0 +1,30 @@
+
+import dotenv from 'dotenv'
+
+import connectDB from "./db/index.js";
+dotenv.config({path: "./env"})
+
+connectDB()
+
+
+/*
+import express from 'express';
+const app = express();
+
+( async ()=>{
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URL}/${DB_Chai}`)
+        app.on('error', ()=>{
+            console.log(Error)
+            throw error
+        })
+        app.listen(process.env.PORT, ()=>{
+            console.log(`App is listioning on port: ${process.env.PORT}`)
+        })
+    } catch (error) {
+        console.error(`Error: ${error}`)
+        throw error
+    }
+
+} )()
+*/
